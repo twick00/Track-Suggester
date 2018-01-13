a5272ec3fce5201df691705e26d5740aec515c81
Tyler Wickline <twick00@gmail.com>
2018-01-13 13:11:16 -0800

    Corrects display of questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,40 @@
-var questionInfo = 
-    [   
-        {"question":"question1", "answer":["answer1","answer2","answer3"]},
-       {"question":"question2", "answer":["answer1","answer2","answer3","answer4","answer5"]},
-       {"":"","":[]}
-    ]
+var questionInfo = [{
+        "question": "question1",
+        "answer": ["answer1", "answer2", "answer3"]
+    },
+    {
+        "question": "question2",
+        "answer": ["answer1", "answer2", "answer3", "answer4", "answer5"]
+    },
+    {
+        "": "",
+        "": []
+    }
+]
 
-    var questionPanel = 
-    '<div class="container">' +
-        '<div class="panel panel-default">'+
-            '<div class="panel-heading">' +
-                '<h3>Test Heading</h3>' +
-            '</div>' +
-            '<div class="panel-body">' +
-                '<p>Test Body</p>' +
-            '</div>' +
+var questionPanel =
+        '<div class="container">' +
+        '<div class="panel panel-default">' +
+        '<div class="panel-heading">' +
+        '<h3 class="question">' +
+        '</h3>' +
         '</div>' +
-    '</div>';
-
+        '<div class="panel-body">' +
+        '<p class="answer">' +
+        '</p>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
 
 // Syntax is: "questionInfo[0].question" to access question1 and "questionInfo[0].answer[n]" to access answer[n]
 //Note to John: I'm aware 'q' isn't a good descripter but I think its pretty straight-forward.
 //Thanks to https://stackoverflow.com/questions/1078118/how-do-i-iterate-over-a-json-structure for reference
-$(document).ready(function() {
-    $(document.body).append(questionPanel);
+$(document).ready(appendQuestions)
 
-    for (var i = 0; i < questionInfo.length; i++)
-    {
-       $(document.body).append(questionPanel);
+function appendQuestions() {
+    for (var i = 0; i < questionInfo.length; i++) {
+        $(document.body).append(questionPanel);
+        $(".question:last").append(questionInfo[i].question);
+        $(".answer:last").append(questionInfo[i].answer);
     }
-})
+}
