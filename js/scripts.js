f2d8facd5be9a0316e3824c5241da5dd370e68a5
Tyler Wickline <twick00@gmail.com>
2018-01-13 12:20:34 -0800

    Adds some scripts. Figured out correct object syntax.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var questionInfo = 
+    [   
+    {"question":"question1", "answer":["answer1","answer2","answer3"]},
+       {"question":"question2", "answer":["answer1","answer2","answer3","answer4","answer5"]},
+       {"":"","":[]}
+    ]
+// Syntax is: "questionInfo.q[0].question" to access question1 and "questionInfo.q[0].answer[n]" to access answer[n]
+//Note to John: I'm aware 'q' isn't a good descripter but I think its pretty straight-forward.
+$(document).ready(function() {
+    for (var key in questionInfo.q)
+    {
+        for ()
+    }
+    q[0].question 
+
+
+
+
+    questionInfo.questions.question1[0];
+})
