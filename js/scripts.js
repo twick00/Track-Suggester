7abae0378f0215bd9c1e1f19de71b2b9fc615941
Tyler Wickline <twick00@gmail.com>
2018-01-13 17:34:10 -0800

    Adds more logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,45 @@
 var questionInfo = [{
-        "question": "question1",
-        "answer": [["answer1", "track1"],
-            ["answer2", "track2"],
-            ["answer3", "track3"],
-            ["answer4", "track4"]
+        "question": "What sounds better to you?",
+        "answer": [
+            ["Back-end.", ["C#", "Go", "Java", "Python", "Ruby", "Node.js"]],
+            ["Front-end.", ["HTML", "CSS", "JavaScript"]],
+            ["Full-stack.", ["C#", "Go", "Java", "Python", "Ruby", "Node.js", "HTML", "CSS", "JavaScript", "C"]],
+            ["I don't know! (And thats ok)", []]
         ],
-        "name": "Test1"
+        "value": 3
     },
     {
-        "question": "question1",
+        "question": "Do you like Open Source languages?",
         "answer": [
-            ["answer1", "track1"],
-            ["answer2", "track2"],
-            ["answer3", "track3"],
-            ["answer4", "track4"]
+            ["Its a requirement.", ["Java", "PHP", "Python", "C#"]],
+            ["It's preferable.", ["Java", "PHP", "Python", "C#","Java","PHP","Ruby"]],
+            ["It doesn't matter to me.", ["C#", "Go", "Java", "Python", "Ruby", "Node.js", "HTML", "CSS", "JavaScript","C"]],
+            ["I don't understand.", []],
         ],
-        "name": "Test2"
-    },
-    {
-        "question": "question1",
+        "value": 2
+    }, {
+        "question": "How much support do you want from the community?",
         "answer": [
-            ["answer1", "track1"],
-            ["answer2", "track2"],
-            ["answer3", "track3"],
-            ["answer4", "track4"]
+            ["A lot!", ["Java", "Python","JavaScript","Ruby"]],
+            ["I'll manage either way.", ["C#", "Go", "Java","Python","JavaScript","Ruby","PHP","CSS"]],
+            ["I don't need community support.", ["Go", "C"]]
         ],
-        "value": 0,
-        "name": "Test3"
+        "value": 1
+    }, {
+        "question": "Do you like the support of large companies behind a language?",
+        "answer": [
+            ["Absolutely.", ["C#", "Go","JavaScript","Java"]],
+            ["I prefer to not involve large companies.", ["Ruby","PHP","CSS","Python",]]
+        ],
+        "value": 2
+    }, {
+        "question": "Would you rather use newer languages?",
+        "answer": [
+            ["Yes!", ["Go", "C#","Ruby",]],
+            ["I don't mind, as long as its simple.", ["Python", "Go", "HTML","CSS"]],
+            ["I prefer older, better supported languages.", ["C", "HTML","CSS","Java",""]],
+        ],
+        "value": 2
     }
 ]
 
@@ -55,11 +68,19 @@ var answerRadio =
         '</label>' +
         '</div>'
     );
+var themes = 
+(
+    '<div class="container-fluid">' +
+    '<div class="row centered-form center-block">' +
+    '<button class="btn btn-primary btn-lg center-block" id="dark-theme">Dark Theme</button>' +
+    '<button class="btn btn-primary btn-lg center-block" id="light-theme">Light Theme</button>' +
+    '<button class="btn btn-primary btn-lg center-block" id="submit">Submit</button>'+
+    '</div>' +
+    '</div>'
+)
 
 
 //<input type="radio" name="activity" value="sonic"> Running
-$(document).ready(appendQuestions)
-
 function appendQuestions() {
     $(document.body).prepend('<div class="container">');
 
@@ -68,11 +89,44 @@ function appendQuestions() {
         $(".question:last").append(questionInfo[i].question);
         for (var o = 0; o < questionInfo[i].answer.length; o++) {
             $(".answer:last").append(answerRadio);
-            $("input:last").attr("name", questionInfo[i].name);
-            $("input:last").attr("value", questionInfo[i].value)
+            $("input:last").attr("name", name + i); //Simplistic unique IDs
+            $("input:last").attr("value", o)
             $("label:last").append(questionInfo[i].answer[o][0]);
         }
+
     }
 
     $(document.body).append("</div>"); //Must go last, this closes the container class 
 }
+$(document).ready(function() {
+    $(document.body).prepend('<div class="container">');
+    $(".jumbotron").append(appendQuestions)
+    $(document.body).append(themes);
+    $(document.body).append("</div>"); //Must go last, this closes the container class 
+
+    $("#dark-theme").click(function() {
+    $("body").removeClass("light-body");
+    $("body").addClass("dark-body");
+    $(".panel-body").removeClass("light-panel-body");
+    $(".panel-body").addClass("dark-panel-body");
+    $(".panel-default>.panel-heading").removeClass("light-panel-heading");
+    $(".panel-default>.panel-heading").addClass("dark-panel-heading");
+    $("#dark-theme").hide();
+    $("#light-theme").show();
+    });
+
+$("#light-theme").click(function() {
+    $("body").removeClass("dark-body");
+    $("body").addClass("light-body");
+    $(".panel-body").removeClass("dark-panel-body");
+    $(".panel-body").addClass("light-panel-body");
+    $(".panel-default>.panel-heading").removeClass("dark-panel-heading");
+    $(".panel-default>.panel-heading").addClass("light-panel-heading");
+    $("#light-theme").hide();
+    $("#dark-theme").show();
+    });
+
+$("#submit").click(function() {
+    
+});
+});
